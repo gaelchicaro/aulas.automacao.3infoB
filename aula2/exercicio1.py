be4bcee84f5b91bcad8c4478606c06839dab89b8
Gael Chicaro <gaelchicaro@gmail.com>
2026-03-23 07:30:30 -0300

    Aula2-Exercicio

--- aula2/exercicio1.py
+++ aula2/exercicio1.py
@@ -4,14 +4,14 @@ nota1 = float(input("Digite sua primeira nota"))
 nota2 = float(input("Digite sua segunda nota"))
 
 #processamento
-media1 = (nota1+nota2)/2
+media = (nota1+nota2)/2
 
-if media1 >= 6:
+if media >= 6:
     print("Aprovado")
 else:
     nota3 = float(input('Digite a nota do exame final'))
-    mediaf = (media1 + nota3)/2
-    if mediaf >= 6:
+    media = (media + nota3)/2
+    if media >= 6:
         print('Aprovado')
     else:
         print('Reprovado')
